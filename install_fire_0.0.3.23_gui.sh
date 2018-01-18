6387af7de9779b9399e8dc82e517f903336a903f
Shaglama <shaglama@gmail.com>
2018-01-18 11:47:49 -0500

    test dont use

--- install_fire_0.0.3.23_gui.sh
+++ install_fire_0.0.3.23_gui.sh
@@ -210,7 +210,7 @@ function installFire(){
 	rm $installer
 }
 ########## PROGRAM ##################################################
-fireVersion="0.0.3.22"
+fireVersion="0.0.3.23"
 fireDir=$(mktemp -d -t fireTemp.XXXXXXXXXX ) #####security versiontmp.XXXXXXXXXX)
 ans=$fireDir/gui
 step="welcome"
