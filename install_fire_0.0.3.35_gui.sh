eebc052f50a8645946e279bff4cf0bdaecb4669f
Shaglama <shaglama@gmail.com>
2018-01-18 16:24:54 -0500

    bug fixes and updated some comments

--- install_fire_0.0.3.35_gui.sh
+++ install_fire_0.0.3.35_gui.sh
@@ -212,7 +212,7 @@ function installFire(){
 	rm $installer
 }
 ########## PROGRAM ##################################################
-fireVersion="0.0.3.34"
+fireVersion="0.0.3.35"
 fireDir=$(mktemp -d -t fireTemp.XXXXXXXXXX ) #####security versiontmp.XXXXXXXXXX)
 ans=$fireDir/gui
 step="welcome"
