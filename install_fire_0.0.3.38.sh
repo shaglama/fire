830a044a936e3d43c2fa3fa839a9f68a8dc2d3da
Shaglama <shaglama@gmail.com>
2018-01-18 17:18:27 -0500

    fixed issue with screen not being killed

--- install_fire_0.0.3.38.sh
+++ install_fire_0.0.3.38.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #fire install scrip
-#version 0.0.3.37
+#version 0.0.3.38
 #Janurary 18, 2018
 #Randy Hoggard
 
@@ -147,7 +147,7 @@ function split() {
 #%%%%%%%% Install fire %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 script=`readlink -f $0`
 scriptDir=`dirname $(readlink -f $0)`
-version="0.0.3.37"
+version="0.0.3.38"
 file="fire_$version.tar.gz"
 ##get any arguments for installer
 while [[ $# -gt 0 ]]; do
--- install_fire_0.0.3.38_gui.sh
+++ install_fire_0.0.3.38_gui.sh
@@ -212,7 +212,7 @@ function installFire(){
 	rm $installer
 }
 ########## PROGRAM ##################################################
-fireVersion="0.0.3.37"
+fireVersion="0.0.3.38"
 fireDir=$(mktemp -d -t fireTemp.XXXXXXXXXX ) #####security versiontmp.XXXXXXXXXX)
 ans=$fireDir/gui
 step="welcome"
